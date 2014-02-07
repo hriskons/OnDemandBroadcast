5e093cc9afca72f6f7c63d58d1ea948354c950b7
hriskons <ckonstantas@gmail.com>
2014-02-07 18:57:26 +0200

    This is the folder that will contain all the src files of the code

---
+++ src/request_list.h
@@ -0,0 +1,28 @@
+#ifndef REQUEST_LIST_H
+#define REQUEST_LIST_H
+
+typedef struct req_t{
+	int client_id;
+	int *req_objects;
+	int req_size;
+	int s_time_stamp;
+	int deadline;
+
+	struct req_t * next;
+
+}REQ_T;
+
+typedef struct req_list{
+	int num_req; /* How many objects will be in the list */
+	int req_num_clients;
+	
+	REQ_T *head; /* The header pointer for the first list */
+
+}REQ_LIST_T;
+
+REQ_LIST_T* read_req_list(char *filename);
+
+void free_req_list(REQ_LIST_T * req_list);
+
+
+#endif
